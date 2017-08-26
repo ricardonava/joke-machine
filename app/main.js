1d98c32159b076be675943e5cec192dab575a572
Ricardo <ricardo.a.nava@gmail.com>
2017-08-25 23:54:09 -0700

    Update main.js with base code
    updated with our base code for project

--- app/main.js
+++ app/main.js
@@ -1,10 +1,50 @@
-function component() {
-  var element = document.createElement('div');
+let counter = 0;
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = "Hello, Webpack";
+const url = "https://api.icndb.com/jokes/random/50";
 
-  return element;
+const jokeText = document.querySelector(".joke-text");
+
+async function fetchJokes() {
+  let jokes = [];
+  let data = await (await fetch(url)).json();
+  for (let i = 0; i < 50; i++) {
+    jokes[i] = data.value[i].joke;
+  }
+  return jokes;
 }
 
-document.body.appendChild(component());
+const prevButton = document.querySelector("button[type=button][value=Prev]");
+
+const nextButton = document.querySelector("button[type=button][value=Next]");
+
+const fetchButton = document.querySelector("button[type=button][value=Fetch]");
+
+function prevJoke(jokes) {
+  if (counter === 0) {
+    counter = jokes.length; // place our counter at the end of the array
+  }
+  counter--;
+  return jokes[counter]; // give us back the actual item
+}
+
+function nextJoke(jokes) {
+  counter++;
+  counter %= jokes.length; // start from 0 if we get to the end of the array
+  return jokes[counter]; // give us back the actual item
+}
+
+function printJoke(joke) {
+  jokeText.textContent = joke;
+}
+
+prevButton.addEventListener("click", () => {
+  printJoke(prevJoke(jokes));
+});
+
+nextButton.addEventListener("click", () => {
+  printJoke(nextJoke(jokes));
+});
+
+fetchButton.addEventListener("click", () => {
+  fetchJokes().then(data => (jokes = data));
+});
