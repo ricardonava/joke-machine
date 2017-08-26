4b32fe69db9fec4f8b56a4164fe6a97f9ee547c7
Ricardo <ricardo.a.nava@gmail.com>
2017-08-26 16:43:18 -0700

    Add index.js
    added index.js with changes to how the loop works, now we are referencing the length of the keys of the Object we receive from he API

---
+++ app/index.js
@@ -0,0 +1,51 @@
+let counter = 0;
+
+const url = "https://api.icndb.com/jokes/random/50";
+
+const jokeText = document.querySelector(".joke-text");
+
+async function fetchJokes() {
+  let jokes = [];
+  let data = await (await fetch(url)).json();
+  let responseLength = Object.keys(data.value).length;
+  for (let i = 0; i < responseLength; i++) {
+    jokes[i] = data.value[i].joke;
+  }
+  return jokes;
+}
+
+const prevButton = document.querySelector("button[type=button][value=Prev]");
+
+const nextButton = document.querySelector("button[type=button][value=Next]");
+
+const fetchButton = document.querySelector("button[type=button][value=Fetch]");
+
+function prevJoke(jokes) {
+  if (counter === 0) {
+    counter = jokes.length; // place our counter at the end of the array
+  }
+  counter--;
+  return jokes[counter]; // give us back the actual item
+}
+
+function nextJoke(jokes) {
+  counter++;
+  counter %= jokes.length; // start from 0 if we get to the end of the array
+  return jokes[counter]; // give us back the actual item
+}
+
+function printJoke(joke) {
+  jokeText.textContent = joke;
+}
+
+prevButton.addEventListener("click", () => {
+  printJoke(prevJoke(jokes));
+});
+
+nextButton.addEventListener("click", () => {
+  printJoke(nextJoke(jokes));
+});
+
+fetchButton.addEventListener("click", () => {
+  fetchJokes().then(dataFetch => jokes = dataFetch);
+});
